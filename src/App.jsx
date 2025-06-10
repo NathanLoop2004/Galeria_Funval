0b6edbc720fe13008b1931e4579c01847f8fe9fe
NathanLoop2004 <Jinderloop@gmail.com>
2025-06-10 15:06:09 -0400

    Ya tengo los botones

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <Nav_Bar onSearch={setSearchInput}/>
-    <Buttons />
+    <Buttons onButtonSearch={setSearchInput}/>
     <CardContainer search={searchInput}/>
 
     </>
--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
 
-export default function Buttons() {
+
+export default function Buttons({onButtonSearch}) {
+  
+   
+
+      const handleClick = (e) =>{
+        if(onButtonSearch) onButtonSearch(e.target.textContent)
+      }
+
   return (
     <div className="flex gap-4 mx-auto mt-2 mb-5 ml-4 md:ml-125">
-      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow">Montain</button>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow">Beaches</button>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow">Birds</button>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow">Food</button>
+      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow"  onClick={handleClick}>Montain</button>
+      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow"  onClick={handleClick}>Beaches</button>
+      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow"  onClick={handleClick}>Birds</button>
+      <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 md:px-8 py-2 rounded shadow"  onClick={handleClick}>Food</button>
     </div>
   )
 }
