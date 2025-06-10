e1a19263e4e5a11ccf53da2afcf4ec070f347fc8
NathanLoop2004 <Jinderloop@gmail.com>
2025-06-10 18:27:17 -0400

    Ahora si?

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Galeria_fotos_funval/",
+  base: "/Galeria_Funval/",
 })
